8035a58b91e8f130d3cc4c95a7e74c7dc4c1fbf3
erick <naybra1994@gmail.com>
2025-01-14 11:03:27 -0400

    gitultimo

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,14 +1,14 @@
 import Logo from '../components/Home/Logo.jsx';
 import Body from '../components/Home/Body.jsx';
-import Github from '../components/Home/Github.jsx';
+import Github from '/src/components/Home/Github.jsx';
 import Footer from '../components/Home/Footer.jsx';
 
 const Home = () => {
     return (
         <div>
-            <Logo></Logo>
-            <Body></Body>
+            <Logo></Logo> 
             <Github></Github>
+            <Body></Body>
             <Footer></Footer>
             
         </div>
