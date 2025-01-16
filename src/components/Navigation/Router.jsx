3d67585df749d40e85abe661968c3160c6addc75
erick <naybra1994@gmail.com>
2025-01-15 22:03:22 -0400

    hooks animacion

--- src/components/Navigation/Router.jsx
+++ src/components/Navigation/Router.jsx
@@ -3,12 +3,14 @@ import Product from  '../../screens/Product.jsx';
 import About from '../../screens/About.jsx';
 import Think from '../../screens/Think.jsx';
 import Counter from '../Counter/counter.jsx';
+import Login from '../../screens/Login.jsx';
 
 import {Routes, Route} from "react-router";
 const Router = () => {
     return (
         <Routes>
             <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
             <Route path="product" element={<Product />} />
             <Route path="think" element={<Think />} />
             <Route path="about" element={<About />} />
---
+++ src/screens/Login.jsx
@@ -0,0 +1,13 @@
+//import FormLogin from "../components/forms/formLogin.jsx";
+//import FormLoginWithHook from "../components/Forms/FormLoginWithHook.jsx";
+import FormLoginWithMotion from "../Components/Forms/FormLoginWithMotion.jsx";
+
+const Login = () => {
+    return(
+        <>
+        {/* <FormLoginWithHook titleForm={"welcome to fullstack program"} /> */}
+        <FormLoginWithMotion titleForm="Welcome to FullStack program" />
+        </>
+    );
+};
+export default Login;
