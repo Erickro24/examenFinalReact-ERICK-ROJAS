4f84d967aa374dcac1b4fe16f9f2a79754af6450
erick <naybra1994@gmail.com>
2025-01-16 20:35:24 -0400

    github

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ import { BrowserRouter } from 'react-router'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <Provider store={store}>
-          <BrowserRouter>
+          <BrowserRouter basename='/desarrollo-react-rojas/'>
             <App />
           </BrowserRouter>
       </Provider>
