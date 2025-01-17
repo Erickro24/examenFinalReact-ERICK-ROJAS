9b4abc691ae545c3aeac58a20042bc8fed89024e
erick <naybra1994@gmail.com>
2025-01-16 20:58:04 -0400

    github ultimo

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,6 +1,6 @@
 //import FormLogin from "../components/forms/formLogin.jsx";
 //import FormLoginWithHook from "../components/Forms/FormLoginWithHook.jsx";
-import FormLoginWithMotion from "../Components/Forms/FormLoginWithMotion.jsx";
+import FormLoginWithMotion from "../components/Forms/FormLoginWithMotion.jsx";
 
 const Login = () => {
     return(
