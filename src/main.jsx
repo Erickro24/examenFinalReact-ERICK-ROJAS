bd65ab08f26995305c48a651f007ddb316295c4c
erick <naybra1994@gmail.com>
2025-01-25 18:04:21 -0400

    modificaciones

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ import { BrowserRouter } from 'react-router'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <Provider store={store}>
-          <BrowserRouter basename='/desarrollo-react-rojas/'>
+          <BrowserRouter basename='/examenFinalReact-ERICK-ROJAS/'>
             <App />
           </BrowserRouter>
       </Provider>
